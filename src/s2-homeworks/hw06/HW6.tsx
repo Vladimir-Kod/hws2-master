202ca1dc06ff74865e29b5208c87a6215f9d2af3
Vladimir-Kod <kodvv@yandex.ru>
2023-02-21 16:01:23 +0500

    hw6 completed

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,8 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-        setValue('enter text...')
-
+        setValue('some text new text')
+        saveState<string>('hw6-editable-span-value', value)
     }
 
     return (
